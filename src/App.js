0c9746fe7a7483673ce99deb2aa4ea851419975d
Thomas Ender <freisingen@yahoo.com>
2021-03-18 20:03:21 +0100

    cleaned up app and made styled components of all components in separate files. got rid of App.css

--- src/App.js
+++ src/App.js
@@ -1,35 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
-import Coin from './components/Coin/Coin';
-import AccountsBalance from './components/AccountsBalance/AccountsBalance.jsx';
+import React, { Component } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
+import Coinlist from './components/Coinlist/Coinlist';
+import AccountsBalance from './components/AccountsBalance/AccountsBalance';
+import ExchangeHeader from './components/ExchangeHeader/ExchangeHeader';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-          <h1 className="App-Title">Coin Exchange</h1>
-      </header>
-      <h2><AccountsBalance amount={10000} /></h2>
-      <table className="Coin-Table">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Ticker</th>
-            <th>Price</th>
-          </tr>
-        </thead>
-        <tbody>
-        <Coin name="Bitcoin" ticker="BTC" price={9999.99}/>
-        <Coin name="Ethereum" ticker="ETH" price={300.87}/>
-        <Coin name="Tether" ticker="USDT" price={1}/>
-        <Coin name="Ripple" ticker="XRP" price={0.2} />
-        </tbody>
-      </table>
-    </div>
-  );
+import styled from 'styled-components';
+
+const Div = styled.div`
+text-align: center;
+background-color: rgba(11, 11, 114, 0.938);
+color: #cccccc
+`;
+
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      balance: 10000,
+      coinData: [
+        {
+          key: uuidv4(),
+          name: "Bitcoin",
+          ticker: "BTC",
+          price: 9999.99
+        },
+        {
+          key: uuidv4(),
+          name: "Ethereum",
+          ticker: "ETH",
+          price: 1800.25
+        },
+        {
+          key: uuidv4(),
+          name: "Tether",
+          ticker: "USDT",
+          price: 1.0
+        },
+        {
+          key: uuidv4(),
+          name: "Ripple",
+          ticker: "XRP",
+          price: 0.98
+        },
+        {
+          key: uuidv4(),
+          name: "Bitcoin Cash",
+          ticker: "BCH",
+          price: 435.45
+        }
+      ]
+    }
+  }
+
+  render(){
+
+    return (
+      <Div>
+        <ExchangeHeader />
+        <h2><AccountsBalance amount={this.state.balance} /></h2>
+        <Coinlist coinData = {this.state.coinData} />
+      </Div>
+    );
+
+  }
+ 
 }
 
 export default App;
